ede99e5af1bb8a3ae82e13df694b1f7ec16ae424
catsfeeledic <catsfeeledic2142@gmail.com>
2020-05-10 15:55:57 +0800

    Java and Kotlin variable , data type

--- src/main/java/com/tom/Hello.java
+++ src/main/java/com/tom/Hello.java
@@ -6,5 +6,15 @@ public class Hello {
 //        new Person().hello();
         Person p = new Person();
         p.hello();
+        int age = 19;
+        Integer age2 = 19;
+        char c = 'A';
+        Character c2 = 'A';
+        byte b =120;
+        float weight =66.5f;
+        double height = 1.7;
+        boolean adult = true;
+        boolean enroll = false;
+        String name = "Tom";
     }
 }
--- src/main/kotlin/com/tom/Hello.kt
+++ src/main/kotlin/com/tom/Hello.kt
@@ -5,6 +5,11 @@ fun main() {
 //    Human().hello()
     val h = Human()
     h.hello()
+    var age = 19
+    age = 20
+    var weight = 66.5
+    var name : String
+    name = "Hank"
 }
 
 class Human {
